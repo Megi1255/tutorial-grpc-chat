dc2b6b0cab8a8479f251945f87b6858186b760fc
jhpark <jonghun@com2us.com>
2019-03-29 14:18:31 +0900

    gopher

--- server/chat_server.go
+++ server/chat_server.go
@@ -14,7 +14,7 @@ import (
 	"sync"
 )
 
-//go:generate protoc -I./protocol -I/usr/local/include -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:./protocol --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./protocol/chat_service.yaml:./protocol --swagger_out=logtostderr=true,grpc_api_configuration=./protocol/chat_service.yaml:./protocol --csharp_out=./protocol --grpc_out=./protocol --plugin=protoc-gen-grpc=/root/.nuget/packages/grpc.tools/1.19.0/tools/linux_x64/grpc_csharp_plugin chat.proto
+//go:generate protoc -I./protocol -I/usr/local/include -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:./protocol --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./protocol/chat_service.yaml:./protocol --swagger_out=logtostderr=true,grpc_api_configuration=./protocol/chat_service.yaml:./protocol --csharp_out=./protocol --grpc_out=./protocol --plugin=protoc-gen-grpc=/root/.nuget/packages/grpc.tools/1.19.0/tools/linux_x64/grpc_csharp_plugin gopher.proto
 
 type ChatServer struct {
 	Addr      string
